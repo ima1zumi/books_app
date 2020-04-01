4f5b8a0f41d5444caec1c085ffd82139271dff6d
imaizumimr <tesioa@gmail.com>
2020-04-01 18:34:03 +0900

    ログイン、ユーザー登録を任意に変更
    before_action :authenticate_user!を外しログイン、新規登録リンクを追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -4,7 +4,7 @@ require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
   include Warden::Test::Helpers
-  def setup
+  setup do
     Warden.test_mode!
     @user = users(:one)
     login_as(@user, scope: :user)

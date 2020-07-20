bdf5fe5690790c1f0459d1fb6983dc74f7232d21
ima1zumi <tesioa@gmail.com>
2020-07-21 02:06:44 +0900

    bookのシステムテストを追加

---
+++ test/system/books_test.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+
+class BooksTest < ApplicationSystemTestCase
+  setup do
+    signin_user("john@example.com", "password")
+  end
+
+  test "show book list" do
+    visit "/"
+    assert_selector "h1", text: "本一覧"
+  end
+
+  test "create book" do
+    visit "ja/books/new"
+    assert_selector "h1", text: "新規登録"
+    fill_in "タイトル", with: "順列都市"
+    fill_in "メモ", with: "SF"
+    fill_in "著者", with: "グレッグ・イーガン"
+    attach_file "画像", Rails.root.join('app/assets/images/shigoto_zaitaku_cat_woman.png')
+    click_on "登録する"
+
+    assert_text "新規作成しました！"
+  end
+
+  test "create and update book" do
+    visit "ja/books/new"
+    assert_selector "h1", text: "新規登録"
+    fill_in "タイトル", with: "紙の動物園"
+    fill_in "メモ", with: "中国現代SF"
+    fill_in "著者", with: "ケン・リュウ"
+    click_on "登録する"
+
+    assert_text "新規作成しました！"
+    click_on "編集"
+
+    assert_selector "h1", text: "編集"
+    fill_in "タイトル", with: "ソラリス"
+    fill_in "メモ", with: "ファーストコンタクト"
+    fill_in "著者", with: "スタニスワフ・レム"
+    click_on "更新する"
+
+    assert_text "更新しました！"
+  end
+
+  test "destroy book" do
+    visit "ja/books"
+    page.accept_confirm do
+      click_on "削除", match: :first
+    end
+
+    assert_text "削除しました！"
+  end
+end

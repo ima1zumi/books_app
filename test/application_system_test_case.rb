8dc72246f5c9a0e97db9e946db0bf818fb6dfe17
ima1zumi <tesioa@gmail.com>
2020-07-20 15:58:59 +0900

    SigninHelperを追加
    siginin/signoutを共通化するためヘルパーを追加した

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "supports/signin_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include SigninHelper
+
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end

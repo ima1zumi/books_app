09eca388a2ef74898616fe9985d57f27ce6ab05f
ima1zumi <tesioa@gmail.com>
2020-07-20 22:55:45 +0900

    userのシステムテストを修正

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,47 +3,75 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  include Warden::Test::Helpers
-  setup do
-    Warden.test_mode!
-    @user = users(:one)
-    login_as(@user, scope: :user)
+  test "sign in" do
+    visit "ja/users/sign_in"
+
+    fill_in "メールアドレス", with: "john@example.com"
+    fill_in "パスワード", with: "password"
+    click_on "ログイン", match: :first
+
+    assert_text "ログインしました。"
+  end
+
+  test "sign out" do
+    signin_user("john@example.com", "password")
+    visit "/"
+    click_on "ログアウト"
+
+    assert_text "ログアウトしました。"
   end
 
   test "visiting an user" do
-    visit user_path(@user.id)
+    signin_user("john@example.com", "password")
+    visit "ja/users/#{users(:user_1).id}"
     assert_selector "h1", text: "ユーザー情報"
   end
 
+  test "create user" do
+    visit "/"
+    click_on "新規登録"
+
+    fill_in "メールアドレス", with: "bob@example.com"
+    fill_in "名前", with: "Bob"
+    fill_in "パスワード", with: "password"
+    fill_in "パスワード（確認用）", with: "password"
+    click_on "アカウント登録"
+
+    assert_text "アカウント登録が完了しました。"
+  end
+
   test "updating an user" do
-    visit user_path(@user.id)
+    signin_user("john@example.com", "password")
+    visit "ja/users/#{users(:user_1).id}"
     click_on "プロフィール編集"
 
-    assert has_field?("メールアドレス", with: @user.email)
-    assert has_field?("名前", with: @user.name)
+    assert has_field?("メールアドレス", with: "john@example.com")
+    assert has_field?("名前", with: "John Wick")
 
     fill_in "郵便番号", with: "123-4567"
     fill_in "住所", with: "40 Example Street London"
     fill_in "自己紹介", with: "妻と犬が好き"
-    fill_in "現在のパスワード", with: "password"
     click_on "更新"
 
     assert_text "アカウント情報を変更しました。"
   end
 
   test "updating an user failed" do
-    visit user_path(@user.id)
+    signin_user("john@example.com", "password")
+    visit "ja/users/#{users(:user_1).id}"
     click_on "プロフィール編集"
 
-    assert has_field?("メールアドレス", with: @user.email)
-    assert has_field?("名前", with: @user.name)
-
-    fill_in "郵便番号", with: "123-4567"
-    fill_in "住所", with: "40 Example Street London"
-    fill_in "自己紹介", with: "妻と犬が好き"
-    fill_in "現在のパスワード", with: ""
+    fill_in "名前", with: "a"*31
     click_on "更新"
 
     assert_text "エラーが発生したため ユーザー は保存されませんでした。"
+  end
+
+  test "destroy an user" do
+    signin_user("taikai@example.com", "password")
+    visit "ja/users/edit"
+    page.accept_confirm do
+      click_on "アカウント削除"
+    end
   end
 end
